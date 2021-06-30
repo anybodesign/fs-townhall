a76b2a8e92cfeaf99d43001b876e18a0831f54d8
Thomas Villain <atelier@anybodesign.com>
2021-06-30 17:20:32 +0200

    toolbar search css

--- template-parts/header-toolbar.php
+++ template-parts/header-toolbar.php
@@ -1,6 +1,7 @@
 <?php if ( !defined('ABSPATH') ) die(); ?>
 			
-			<?php if ( has_nav_menu( 'toolbar_menu' ) || get_theme_mod('searchbar') ) : ?>
+		<?php if ( has_nav_menu( 'toolbar_menu' ) || get_theme_mod('searchbar') || get_theme_mod('contrast') ) : ?>
+			
 			<div class="site-toolbar<?php if ( get_theme_mod('searchbar') ) { echo ' has-search'; } ?>">
 				
 				<?php if ( has_nav_menu( 'toolbar_menu' ) ) { ?>
@@ -14,32 +15,33 @@
 				</nav>
 				<?php } ?>
 				
-			<?php if ( get_theme_mod('searchbar') || get_theme_mod('contrast') ) { ?>
+				<?php if ( get_theme_mod('searchbar') || get_theme_mod('contrast') ) { ?>
 				<div class="toolbar-widgets">
 				
-				<?php if ( get_theme_mod('searchbar') ) { ?>
-				<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-				    <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php esc_html_e( 'Enter keyword', 'fs-townhall' ); ?>">
-				    <label for="s"><?php esc_html_e( 'Search for', 'fs-townhall' ); ?></label>
-					<input type="submit" class="action-btn" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'fs-townhall' ); ?>">
-			    </form>
-				<?php } ?>
-				
-				<?php if ( get_theme_mod('contrast') == true ) { ?>
-				<div class="contrast-switch">
-					<button class="toggle-highcontrast" type="button">
-						<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-on.svg" alt="">
-						<span><?php esc_html_e('High contrast', 'fs-townhall'); ?></span>
-					</button>
-					<button class="toggle-remove" type="button">
-						<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-off.svg" alt="">
-						<span><?php esc_html_e('Restore contrast', 'fs-townhall'); ?></span>
-					</button>
+					<?php if ( get_theme_mod('searchbar') ) { ?>
+					<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
+					    <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php esc_html_e( 'Enter keyword', 'fs-townhall' ); ?>">
+					    <label for="s"><?php esc_html_e( 'Search for', 'fs-townhall' ); ?></label>
+						<input type="submit" class="action-btn" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'fs-townhall' ); ?>">
+				    </form>
+					<?php } ?>
+					
+					<?php if ( get_theme_mod('contrast') == true ) { ?>
+					<div class="contrast-switch">
+						<button class="toggle-highcontrast" type="button">
+							<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-on.svg" alt="">
+							<span><?php esc_html_e('High contrast', 'fs-townhall'); ?></span>
+						</button>
+						<button class="toggle-remove" type="button">
+							<img src="<?php echo FS_THEME_URL; ?>/img/ui/contrast-off.svg" alt="">
+							<span><?php esc_html_e('Restore contrast', 'fs-townhall'); ?></span>
+						</button>
+					</div>
+					<?php } ?>
+					
 				</div>
 				<?php } ?>
 				
-				</div>
-			<?php } ?>
+			</div>
 			
-			</div>	
-			<?php endif; ?>	
+		<?php endif; ?>	
