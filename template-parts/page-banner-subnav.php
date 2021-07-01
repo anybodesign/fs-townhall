13a9c1f5ac8d0d602807c0edf4f9ad1ec1976e37
Thomas Villain <atelier@anybodesign.com>
2021-07-01 13:25:57 +0200

    page banner markup

---
+++ template-parts/page-banner-subnav.php
@@ -0,0 +1,28 @@
+<?php if ( !defined('ABSPATH') ) die();
+/**
+ * @package WordPress
+ * @subpackage FS_Townhall
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+		<?php
+			if ( is_page() ) {
+				
+				$parent = $post->post_parent;
+				
+				if ( $parent ) {
+					$children = wp_list_pages("title_li=&child_of=".$parent."&echo=0");
+				} else {
+					$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+				}
+				
+					if ( $children ) { ?>
+					<nav class="sub-pages">
+						<ul class="subpages-list">
+							<?php echo $children; ?>
+						</ul>
+					</nav>
+					<?php }				
+			} 
+		?>
--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -13,7 +13,7 @@
 ?>
 					<div class="page-banner"<?php if ( $has_bg ) { fs_bg_img(); } ?>>
 						<div class="inner">
-						
+							
 							<h1 class="page-title">
 							<?php 	
 								if ( is_home() && ! is_front_page() ) {
@@ -31,17 +31,8 @@
 							</h1>
 							
 							<?php 
-								if ( is_page_template( 'pagecustom-maintenance.php' ) ) {
-									the_content(); 
-								} 
-								if ( is_archive() ) {
-									the_archive_description( '<div class="archive-desc">', '</div>' );
-								}
-								if ( is_single() ) {
-									while ( have_posts() ) : the_post();
-									get_template_part('template-parts/post', 'meta');
-									endwhile;							
-								} 
+								get_template_part( 'template-parts/page-banner', 'subnav' ); 
+								get_template_part( 'template-parts/page-banner', 'desc' ); 
 							?>
 						</div>
 					</div>
