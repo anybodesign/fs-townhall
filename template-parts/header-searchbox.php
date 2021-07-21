cfc5a7962f622ec5c70c864f4dc295ea84e86824
Thomas Villain <atelier@anybodesign.com>
2021-07-21 17:24:03 +0200

    esc_attr fix

--- template-parts/header-searchbox.php
+++ template-parts/header-searchbox.php
@@ -8,7 +8,7 @@
 			<div class="searchbox">
 				
 				<form role="search" method="get" id="searchform" class="searchform animated-search" action="<?php echo home_url( '/' ); ?>">
-				    <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php esc_html_e( 'Enter keyword', 'fs-townhall' ); ?>">
+				    <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="<?php esc_attr_e( 'Enter keyword', 'fs-townhall' ); ?>">
 				    <label for="s"><?php esc_html_e( 'Search for', 'fs-townhall' ); ?></label>
 					<input type="submit" class="action-btn" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'fs-townhall' ); ?>">
 			    </form>
