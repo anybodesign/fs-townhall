67c6fb22d79c40ca1ac713965f4a7970a0399314
Thomas Villain <atelier@anybodesign.com>
2023-11-21 12:45:20 +0100

    Event picture fallback

--- template-parts/event-block.php
+++ template-parts/event-block.php
@@ -10,17 +10,20 @@
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('post-block'); ?>>
 						
-						<?php if ( '' != get_the_post_thumbnail() ) {
-							$img_id = get_post_thumbnail_id();
-							$img_url = wp_get_attachment_image_src( $img_id, 'screen-md' );
-							$img_alt = get_post_meta($img_id, '_wp_attachment_image_alt', true);
-						?>
+						
 						<div class="post-figure">
 							<a href="<?php the_permalink(); ?>" rel="nofollow">
-							<img src="<?php echo $img_url[0]; ?>" alt="<?php echo $img_alt; ?>">
+							<?php if ( '' != get_the_post_thumbnail() ) {
+								$img_id = get_post_thumbnail_id();
+								$img_url = wp_get_attachment_image_src( $img_id, 'screen-md' );
+								$img_alt = get_post_meta($img_id, '_wp_attachment_image_alt', true);
+							?>
+								<img src="<?php echo $img_url[0]; ?>" alt="<?php echo $img_alt; ?>">
+							<?php } else { ?>
+								<img src="<?php echo FS_THEME_URL . '/img/fallback.png'; ?>" alt="">
+							<?php } ?>
 							</a>
 						</div>
-						<?php } ?>
 						
 						<div class="post-content">
 							<header class="post-header">
